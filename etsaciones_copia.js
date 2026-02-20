36821bc9d43b22ecabf1d2e6ccb7f4e27c3d0b6f
librekcesar <140665209+librekcesar@users.noreply.github.com>
2026-02-20 16:53:27 -0600

    Add files via upload

---
+++ etsaciones_copia.js
@@ -0,0 +1,9 @@
+var puntos = {
+"type": "FeatureCollection",
+"name": "etsaciones",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "nombre": "Guadalajara", "direccion": "Av. de las Torres 5500, Camichines, 45580 San Pedro Tlaquepaque, Jal." }, "geometry": { "type": "Point", "coordinates": [ -103.289877784341385, 20.624522187727656 ] } },
+{ "type": "Feature", "properties": { "id": 2, "nombre": "Monterrey", "direccion": "Av Zapopan 282, Los Altos, 64370 Monterrey, N.L." }, "geometry": { "type": "Point", "coordinates": [ -100.373040598695255, 25.715074372430511 ] } }
+]
+}
